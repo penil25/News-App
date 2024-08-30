99bb7e23e3a606df796f49c0c34ca219a23cdd90
penil25 <penilhirapara0808@gmail.com>
2024-08-30 10:02:13 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+// import 'package:flutter_news_app/bloc/news_bloc.dart';
+// import 'package:flutter_news_app/view/splash_screen.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:my_project2/view/splash_screen.dart';
+
+import 'bloc/news_bloc.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<NewsBloc>(create: (BuildContext context) => NewsBloc(),),
+      ],
+      child: MaterialApp(
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: SplashScreen(),
+      ),
+    );
+  }
+}
+
+
+
+const spinkit = SpinKitChasingDots(
+  color: Colors.blue,
+  size: 40.0,
+);
+
+
+
